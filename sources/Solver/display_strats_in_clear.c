706a6d4bcdb3c750b67c3ae15bf2baf367bcc245
Geoff McLane <ubuntu@geoffair.info>
2020-08-20 21:31:42 +0200

    Some defensive code to stop array overrun

--- sources/Solver/display_strats_in_clear.c
+++ sources/Solver/display_strats_in_clear.c
@@ -9,10 +9,31 @@
 #include "display_strats_in_clear.h"
 
 void display_strats_in_clear() {
+#ifdef LOG_IN_OUT
+    printf("--- display_strats_in_clear >>>\n");
+#endif
+
   for (int k = 0; k < n_strats_used; k++) {
-    int level = strats_used[k] / 10;
-    int k_strat = strats_used[k] - level * 10;
-    printf(" '%s'", strat_all_names[level][k_strat]);
+      int used = strats_used[k];
+    int level = used / 10;
+    int k_strat = used - level * 10;
+    if (level < n_levels) {
+        if (k_strat < n_strat_all[level]) {
+            printf(" '%s'", strat_all_names[level][k_strat]);
+        }
+        else {
+            printf("\nError: used %d: k_strat %d not less that %d", used, k_strat, n_strat_all[level]);
+            break;
+        }
+    }
+    else {
+        printf("\nError: used %d: level %d not less that %d", used, level, n_levels);
+        break;
+    }
     }
   printf("\n");
+#ifdef LOG_IN_OUT
+  printf("<<< display_strats_in_clear ---\n");
+#endif
+
   }
